50831e20f4d7e834ec012744e58a6ff7a2195db8
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2023-12-22 00:49:46 +0530

    add faq section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,6 +12,8 @@ import featureImg from "../assets/images/feature-img.png";
 import videoIcon from "../assets/images/video-icon.png";
 import avatarIcon from "../assets/images/avatar-icon.png";
 import DoctorList from "../components/Doctors/DoctorList";
+import faqImg from "../assets/images/faq-img.png";
+import FaqList from "../components/Faq/FaqList";
 const Home = function () {
   return (
     <>
@@ -269,11 +271,29 @@ font-[400]"
           </div>
           <DoctorList />
         </div>
-
-        
       </section>
 
       {/*doctors section ens*/}
+
+      {/*faq section*/}
+      <section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImg} />
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most questions by our beloved patients
+              </h2>
+              <FaqList />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/*faq section end*/}
     </>
   );
 };
