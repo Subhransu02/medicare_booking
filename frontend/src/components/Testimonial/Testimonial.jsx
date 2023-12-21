1935092b430e685e50bc96726698e21f4d1052a0
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2023-12-22 02:26:59 +0530

    Add testimonial section

---
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,201 @@
+// import { Pagination } from "swiper";
+// import { Swiper, SwiperSlide } from "swiper/react";
+import patientAvatar from "../../assets/images/patient-avatar.png";
+// import "swiper/css";
+// import "swiper/css/pagination";
+import { HiStar } from "react-icons/hi";
+const Testimonial = () => {
+  return (
+    <div className="mt-[30px] lg:mt-[55px] flex-row items-center gap-[13px]">
+      {/* <Swiper
+        modules={[Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        }}
+      >
+        <SwiperSlide>
+          
+        </SwiperSlide>
+      </Swiper> */}
+
+      <div className="py-[30px] px-5 rounded-3">
+        <div className="flex items-center gap-[13px]">
+          <img src={patientAvatar} alt="" />
+          <div>
+            <h4 className="text-[18px] leading-[30px] font-semibold text-headingColor">
+              Muhibur Rahmman
+            </h4>
+            <div className="flex items-center gap-[2px]">
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+            </div>
+          </div>
+        </div>
+
+        <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
+          "I have taken medical services from them. They treat so well ans they
+          are providing the best medical services."
+        </p>
+      </div>
+
+
+      <div className="py-[30px] px-5 rounded-3">
+        <div className="flex items-center gap-[13px]">
+          <img src={patientAvatar} alt="" />
+          <div>
+            <h4 className="text-[18px] leading-[30px] font-semibold text-headingColor">
+              Muhibur Rahmman
+            </h4>
+            <div className="flex items-center gap-[2px]">
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+            </div>
+          </div>
+        </div>
+
+        <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
+          "I have taken medical services from them. They treat so well ans they
+          are providing the best medical services."
+        </p>
+      </div>
+
+      <div className="py-[30px] px-5 rounded-3">
+        <div className="flex items-center gap-[13px]">
+          <img src={patientAvatar} alt="" />
+          <div>
+            <h4 className="text-[18px] leading-[30px] font-semibold text-headingColor">
+              Muhibur Rahmman
+            </h4>
+            <div className="flex items-center gap-[2px]">
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+            </div>
+          </div>
+        </div>
+
+        <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
+          "I have taken medical services from them. They treat so well ans they
+          are providing the best medical services."
+        </p>
+      </div>
+
+      <div className="py-[30px] px-5 rounded-3">
+        <div className="flex items-center gap-[13px]">
+          <img src={patientAvatar} alt="" />
+          <div>
+            <h4 className="text-[18px] leading-[30px] font-semibold text-headingColor">
+              Muhibur Rahmman
+            </h4>
+            <div className="flex items-center gap-[2px]">
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+            </div>
+          </div>
+        </div>
+
+        <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
+          "I have taken medical services from them. They treat so well ans they
+          are providing the best medical services."
+        </p>
+      </div>
+
+      <div className="py-[30px] px-5 rounded-3">
+        <div className="flex items-center gap-[13px]">
+          <img src={patientAvatar} alt="" />
+          <div>
+            <h4 className="text-[18px] leading-[30px] font-semibold text-headingColor">
+              Muhibur Rahmman
+            </h4>
+            <div className="flex items-center gap-[2px]">
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+            </div>
+          </div>
+        </div>
+
+        <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
+          "I have taken medical services from them. They treat so well ans they
+          are providing the best medical services."
+        </p>
+      </div>
+
+      <div className="py-[30px] px-5 rounded-3">
+        <div className="flex items-center gap-[13px]">
+          <img src={patientAvatar} alt="" />
+          <div>
+            <h4 className="text-[18px] leading-[30px] font-semibold text-headingColor">
+              Muhibur Rahmman
+            </h4>
+            <div className="flex items-center gap-[2px]">
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+            </div>
+          </div>
+        </div>
+
+        <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
+          "I have taken medical services from them. They treat so well ans they
+          are providing the best medical services."
+        </p>
+      </div>
+
+      
+      <div className="py-[30px] px-5 rounded-3">
+        <div className="flex items-center gap-[13px]">
+          <img src={patientAvatar} alt="" />
+          <div>
+            <h4 className="text-[18px] leading-[30px] font-semibold text-headingColor">
+              Muhibur Rahmman
+            </h4>
+            <div className="flex items-center gap-[2px]">
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+              <HiStar className="text-yellowColor w-[18px] h-5" />
+            </div>
+          </div>
+        </div>
+
+        <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
+          "I have taken medical services from them. They treat so well ans they
+          are providing the best medical services."
+        </p>
+      </div>
+      
+    </div>
+  );
+};
+export default Testimonial;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,6 +14,7 @@ import avatarIcon from "../assets/images/avatar-icon.png";
 import DoctorList from "../components/Doctors/DoctorList";
 import faqImg from "../assets/images/faq-img.png";
 import FaqList from "../components/Faq/FaqList";
+import Testimonial from "../components/Testimonial/Testimonial";
 const Home = function () {
   return (
     <>
@@ -294,6 +295,22 @@ font-[400]"
       </section>
 
       {/*faq section end*/}
+
+      {/*testimonial*/}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">What our patient say</h2>
+            <p className="text__para text-center">
+              World-class care for everyone. Our health System offers unmatched,
+              expert health care.
+            </p>
+          </div>
+          <Testimonial />
+        </div>
+      </section>
+
+      {/*testimonial end*/}
     </>
   );
 };
