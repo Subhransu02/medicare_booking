364b9730de30974f6d3596a24869ddcd8ceffb1d
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2023-12-24 02:32:17 +0530

    add authenticate & restrict

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -5,11 +5,12 @@ import {
   getAllUsers,
   getSingleUser,
 } from "../Controllers/userController.js";
+import { authenticate, restrict } from "../auth/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", getAllUsers);
-router.get("/:id", getSingleUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.get("/", authenticate, restrict(["admin"]), getAllUsers);
+router.get("/:id", authenticate, restrict(["patient"]), getSingleUser);
+router.put("/:id", authenticate, restrict(["patient"]), updateUser);
+router.delete("/:id", authenticate, restrict(["patient"]), deleteUser);
 export default router;
