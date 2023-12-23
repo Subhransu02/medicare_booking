7af40ff20a0b6466edd27f36e38fa5df4b14c3d7
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2023-12-24 02:02:48 +0530

    add user controller

---
+++ backend/Routes/user.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getAllUsers,
+  getSingleUser,
+} from "../Controllers/userController.js";
+
+const router = express.Router();
+
+router.get("/", getAllUsers);
+router.get("/:id", getSingleUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+export default router;
